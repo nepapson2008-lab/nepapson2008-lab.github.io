8e615e6ae7c408f513197a735b41438eb623e7af
nepapson2008-lab <nepapson2008@gmail.com>
2026-07-23 15:01:03 +0700

    Fix typo in portfolio log message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //เพิ่ม Javascript สำหรับการโต้ตอลกับผู้ใช้(Optionl)
 document.addeventListener('DOMContenLoaded',function(){
-  consola.log('Portfolio ของ NETAPSON พร้อมแล้ว!');
+  consola.log('Portfolio ของ NEPAPSON พร้อมแล้ว!');
 });
