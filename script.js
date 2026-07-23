43811e84b22385f90b835a83e82ef98d5fee0626
nepapson2008-lab <nepapson2008@gmail.com>
2026-07-23 15:14:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//เพิ่ม Javascript สำหรับการโต้ตอลกับผู้ใช้(Optionl)
+//เพิ่ม Javascript สำหรับการโต้ตอลกับผู้ใช้(Optional)
 document.addeventListener('DOMContenLoaded',function(){
   consola.log('Portfolio ของ NEPAPSON พร้อมแล้ว!');
 });
